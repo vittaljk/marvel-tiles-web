0af10a16347ab99770362e672333bd073a54cd52
vittaljk <vittaljk@gmail.com>
2025-11-07 01:25:00 +0530

    mobile video done

--- components/Atoms/HeroBanner/HeroBanner.tsx
+++ components/Atoms/HeroBanner/HeroBanner.tsx
@@ -2,21 +2,34 @@ import React from 'react'
 
 interface HeroBannerProps {
     bannerVideoUrl?: string;
+    bannerVideoMobileUrl?: string;
 }
 
-function HeroBanner({ bannerVideoUrl }: HeroBannerProps) {
+function HeroBanner({ bannerVideoUrl, bannerVideoMobileUrl }: HeroBannerProps) {
     return (
         <div className="relative w-full h-[75vh] overflow-hidden">
-            <video
-                autoPlay
-                loop
-                muted
-                playsInline
-                className="w-full h-full object-cover"
-            >
-                <source src={bannerVideoUrl} type="video/mp4" />
-            </video>
-
+            <div className='hidden md:block w-full h-full'>
+                <video
+                    autoPlay
+                    loop
+                    muted
+                    playsInline
+                    className="w-full h-full object-cover"
+                >
+                    <source src={bannerVideoUrl} type="video/mp4" />
+                </video>
+            </div>
+            <div className='md:hidden w-full h-full'>
+                <video
+                    autoPlay
+                    loop
+                    muted
+                    playsInline
+                    className="w-full h-full object-cover"
+                >
+                    <source src={bannerVideoMobileUrl} type="video/mp4" />
+                </video>
+            </div>
             <div className="absolute inset-0 bg-black/40" />
         </div>
     )
@@ -26,4 +39,5 @@ export default HeroBanner
 
 HeroBanner.defaultProps = {
     bannerVideoUrl: "https://res.cloudinary.com/duy0961om/video/upload/v1762016623/herobannervideo_3a7uknCH_vbdc6x.mp4",
+    bannerVideoMobileUrl: "https://res.cloudinary.com/duy0961om/video/upload/v1762458720/mobile-reel_kpQEIV7d_ttfs0v.mp4",
 }
